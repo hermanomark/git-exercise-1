b14c849e293fc854be2b1e9b6ab569ffc43e4f2c
Mark Hermano <hermano.mark.mh@gmail.com>
2018-03-09 11:20:12 +0800

    first commit

---
+++ my_first_and_last_name.rb
@@ -0,0 +1,15 @@
+my_firstname = "Mark"
+my_lastname = "Hermano"
+email = "hermano.mark@yahoo.com"
+
+puts "Firstname: #{my_firstname}"
+puts "Lastname: #{my_lastname}"
+puts "Email: #{email}"
+
+if my_firstname == "Mark Jason" then
+  puts "This is complete"
+if else my_firstname == "Mark"
+  puts "Not complete"
+if else my_firstname == "Jason"
+  puts "Not complete"
+end
