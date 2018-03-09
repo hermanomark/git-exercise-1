b4dbeb520307b9438bd0a085784b93216f3659a3
Mark Hermano <hermano.mark.mh@gmail.com>
2018-03-09 11:26:16 +0800

    fix if else statement

--- my_first_and_last_name.rb
+++ my_first_and_last_name.rb
@@ -1,4 +1,4 @@
-my_firstname = "Mark"
+my_firstname = "Mark Jason"
 my_lastname = "Hermano"
 email = "hermano.mark@yahoo.com"
 
@@ -6,10 +6,13 @@ puts "Firstname: #{my_firstname}"
 puts "Lastname: #{my_lastname}"
 puts "Email: #{email}"
 
+
 if my_firstname == "Mark Jason" then
   puts "This is complete"
-if else my_firstname == "Mark"
+elsif my_firstname == "Mark"
   puts "Not complete"
-if else my_firstname == "Jason"
+elsif my_firstname == "Jason"
   puts "Not complete"
+else
+  puts "No value"
 end
